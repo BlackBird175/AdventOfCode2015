609e28fdc300e5b78872ea56363bf0f4684a34b4
KANDIL <mohamed.kandil@etu.univ-orleans.fr>
2024-12-21 02:48:17 +0100

    Day 2 : little refacto and update of instructions for part two

--- src/main/java/fr/mo/day2/InstructionsDay2.md
+++ src/main/java/fr/mo/day2/InstructionsDay2.md
@@ -38,3 +38,32 @@ C'est l'aire du plus petit côté :
 
 Tous les nombres de la liste des lutins sont exprimés en pieds. 
 Combien de mètres carrés de papier d'emballage doivent-ils commander ?
+
+--- Part Two ---
+
+The elves are also running low on ribbon. Ribbon is all the same width, so they only have to worry about the length 
+they need to order, which they would again like to be exact.
+
+The ribbon required to wrap a present is the shortest distance around its sides, or the smallest perimeter of any one 
+face. Each present also requires a bow made out of ribbon as well; the feet of ribbon required for the perfect bow is 
+equal to the cubic feet of volume of the present. Don't ask how they tie the bow, though; they'll never tell.
+
+For example:
+
+    A present with dimensions 2x3x4 requires 2+2+3+3 = 10 feet of ribbon to wrap the present 
+    plus 2*3*4 = 24 feet of ribbon for the bow, for a total of 34 feet.
+    
+    A present with dimensions 1x1x10 requires 1+1+1+1 = 4 feet of ribbon to wrap the present plus 
+    1*1*10 = 10 feet of ribbon for the bow, for a total of 14 feet.
+
+How many total feet of ribbon should they order?
+
+Your puzzle answer was 3783758.
+
+Both parts of this puzzle are complete! They provide two gold stars: **
+
+At this point, you should return to your Advent calendar and try another puzzle.
+
+If you still want to see it, you can get your puzzle input.
+
+You can also [Shareon Bluesky Twitter Mastodon] this puzzle.
--- src/main/java/fr/mo/day2/Model.java
+++ src/main/java/fr/mo/day2/Model.java
@@ -12,36 +12,33 @@ public class Model {
 
     public void readFile() throws IOException {
 
+        //BufferedReader with file reader to read into the file
         BufferedReader reader = new BufferedReader(new FileReader("/home/kandinsky/Documents/CodeExos" +
-                "/AdventOfCode2015/src/main/java/fr/mo/day2/Operations.txt")) ;
-        int finalResult = 0;
-        int rubberResult = 0;
+                "/AdventOfCode2015/src/main/java/fr/mo/day2/operations.txt")) ;
 
+        int paperNeeded = 0;
+        int ribbonNeeded = 0;
+
+        //For every line in the file, until the end of it
         for(String line = reader.readLine(); line!=null; line = reader.readLine()) {
 
             List<Integer> dimensions  = new ArrayList<>();
             String [] dimensionsStr = line.split("x");
 
-            int a = Integer.parseInt(dimensionsStr[0]);
-            int b = Integer.parseInt(dimensionsStr[1]);
-            int c = Integer.parseInt(dimensionsStr[2]);
+            dimensions.add(Integer.parseInt(dimensionsStr[0]));
+            dimensions.add(Integer.parseInt(dimensionsStr[1]));
+            dimensions.add(Integer.parseInt(dimensionsStr[2]));
 
-            dimensions.add(a);
-            dimensions.add(b);
-            dimensions.add(c);
-
-
+            //Sort acending order to get the value of extra paper and ribbon needed
             Collections.sort(dimensions);
 
-            rubberResult += (2 * dimensions.get(0) + 2 * dimensions.get(1)) +
+            ribbonNeeded += (2 * dimensions.get(0) + 2 * dimensions.get(1)) +
                     dimensions.get(0) * dimensions.get(1) * dimensions.get(2);
 
-            finalResult += (2 * dimensions.get(0) * dimensions.get(1) + 2 * dimensions.get(1) * dimensions.get(2) +
+            paperNeeded += (2 * dimensions.get(0) * dimensions.get(1) + 2 * dimensions.get(1) * dimensions.get(2) +
                     2 * dimensions.get(2) * dimensions.get(0)) + dimensions.get(0) * dimensions.get(1);
-
         }
-
-        System.out.println("The presents will need " + finalResult + " feets of paper and " + rubberResult +
+        System.out.println("The presents will need " + paperNeeded + " feets of paper and " + ribbonNeeded +
                 " feets of rubber");
     }
 }
